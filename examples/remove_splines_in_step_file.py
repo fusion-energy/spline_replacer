4e2a5658e8416f055c012e5a23ae8e65fe1a5c5d
Jonathan Shimwell <mail@jshimwell.com>
2024-06-05 00:22:10 +0100

    filename typo

--- examples/remove_splines_in_step_file.py
+++ examples/remove_splines_in_step_file.py
@@ -1,7 +1,7 @@
 from spline_replacer import replace_splines
 
 spline_free = replace_splines(
-    solids="solid_with_spines.step",
+    solids="solid_with_splines.step",
     tolerance = 0.1,
     theAngularDeflection = 0.5,
     theCurvatureDeflection = 0.01,
