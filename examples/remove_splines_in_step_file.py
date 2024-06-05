40d1d54c5036dfed0ed83939c05acc757ed955d8
Jonathan Shimwell <mail@jshimwell.com>
2024-06-05 01:06:55 +0100

    lower number of triangles

--- examples/remove_splines_in_step_file.py
+++ examples/remove_splines_in_step_file.py
@@ -2,7 +2,7 @@ from spline_replacer import replace_splines
 
 spline_free = replace_splines(
     solids="solid_with_splines.step",
-    tolerance = 0.1,
+    tolerance = 20,
     theAngularDeflection = 0.5,
     theCurvatureDeflection = 0.01,
 )
